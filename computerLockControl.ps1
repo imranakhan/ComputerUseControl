400f85a1545de996cb83668805a867cf765e7f2d
Imran Khan <imranak123@gmail.com>
2020-07-26 11:58:32 -0500

    Restart schedule script and windows task added

---
+++ computerLockControl.ps1
@@ -0,0 +1,18 @@
+#Check to see if the LogonUI process is running. It is running when the computer is Locked and UI is showing
+$ProcessName = "LogonUI"
+if((get-process $ProcessName -ErrorAction SilentlyContinue) -eq $Null) { 
+    echo "Process is not running" 
+    #Lock the computer
+    rundll32.exe user32.dll,LockWorkStation
+}
+else { 
+    echo "Process is running" 
+    #Do Nothing
+}
+
+
+
+#while(1)
+#{
+#   start-sleep -seconds 600
+#}
