5d38718b391a057811b40c3b7bca610b6505a035
Imran Khan <imranak123@gmail.com>
2020-08-12 22:14:12 -0500

    Add support for workout images too

--- fitnessImage.ps1
+++ fitnessImage.ps1
@@ -1,14 +1,21 @@
 $token = "1389223003:AAGSYOvwwaDXGiZ7ARjrJTVxeCYGl3FjWy0"
 $chatId = -498768087
 $wallhaven_key = "mokfnXGDSHj1m8oTcOkq8fQguF2q29e4"
-$query = "muscles -women" #fitness model
+$queryMen = "muscles -women" #fitness model
+$queryWorkout = "workout" #Includes Women
 $category = "001"
 $purity = "100"
 
 $page = 1#Get-Random -Maximum 1 -Minimum 1
-$randomNumber = Get-Random -Maximum 60
+$pageNumber = Get-Random -Maximum 3 -Minimum 1
+$pageMax = 64 #Since there are 64 images per page
 
-$fitnessUrl = "https://wallhaven.cc/api/v1/search?apikey=$wallhaven_key&q=$query&categories=$category&purity=$purity&page=$page"
+if($pageNumber -eq 3) {
+    $pageMax = 20 #Since the 3rd page for this workout query has 20 images
+}
+$randomNumber = Get-Random -Maximum $pageMax -Minimum 1
+
+$fitnessUrl = "https://wallhaven.cc/api/v1/search?apikey=$wallhaven_key&q=$queryWorkout&categories=$category&purity=$purity&page=$page"
 
 Write-Host $fitnessUrl
 
