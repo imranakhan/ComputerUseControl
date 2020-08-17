73ffd6d02ed47753bc42927700462f69832ab026
Imran Khan <imranak123@gmail.com>
2020-08-16 23:50:05 -0500

    Clean up

--- RetrieveDailyQuotes.ps1
+++ RetrieveDailyQuotes.ps1
@@ -6,19 +6,20 @@ $categories = @('inspire','management','sports', 'life', 'funny', 'love', 'art',
 
 foreach ($category in $categories) {
 
-    #$quotesUrl = "https://quotes.rest/qod?language=en&category=$category"
+    #Get Quote from Quote of the day API
+    $quotesUrl = "https://quotes.rest/qod?language=en&category=$category"
 
-    #$object = Invoke-RestMethod -Method Get -Uri $quotesUrl -contenttype 'application/json'
+    $object = Invoke-RestMethod -Method Get -Uri $quotesUrl -contenttype 'application/json'
     
     #Get the Quote and the Author from the response
-    #$quote = $object.contents.quotes[0].quote
-    #$author = $object.contents.quotes[0].author
+    $quote = $object.contents.quotes[0].quote
+    $author = $object.contents.quotes[0].author
     
-    #$dailyQuote = "$quote - $author"
+    $dailyQuote = "$quote - $author"
 
     $filename = "$category.txt"
     
-    Add-Content ".\Quotes\$filename" "Hello World" #$dailyQuote
+    Add-Content ".\Quotes\$filename" $dailyQuote
 }
 
 
